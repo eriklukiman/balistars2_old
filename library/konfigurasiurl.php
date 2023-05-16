a17ad787cd7763178082d1678fdd3a253c99be75
eriklukiman <erik.lukiman@gmail.com>
2023-05-16 15:17:55 +0700

    change to balistars2
    change timezone to WITA, not singapore

--- library/konfigurasiurl.php
+++ library/konfigurasiurl.php
@@ -1,8 +1,8 @@
 <?php
-date_default_timezone_set("Asia/Singapore");
+date_default_timezone_set("Asia/Makassar");
 
-$BASE_URL_HTML = '/balistars1';
-$BASE_URL_PHP  = $_SERVER['DOCUMENT_ROOT'].'/balistars1';
+$BASE_URL_HTML = '/balistars2';
+$BASE_URL_PHP  = $_SERVER['DOCUMENT_ROOT'].'/balistars2';
 
 //$BASE_URL_HTML = '/balistars/balistars1';
 //$BASE_URL_PHP  = '/var/www/html/balistars/balistars1';
